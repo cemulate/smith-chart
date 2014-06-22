abf36f37c44a29cea2fa371e296a94c4b2f31439
Chase Meadors <c.ed.mead@gmail.com>
2014-06-21 22:43:29 -0500

    setup gh-pages

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn smith-chart:app 
--- smith-chart.py
+++ smith-chart.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return open('templates/index.html', 'rb').read()
+    return open('index.html', 'rb').read()
     
 if __name__ == '__main__':
     app.run('127.0.0.1', 8000, True)
